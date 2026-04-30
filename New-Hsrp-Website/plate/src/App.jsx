3bb77e745a6fbd0ef0cf16af3f453a39038ab1bc
Ayush Sharma <ayushsharma00123456@gmail.com>
2026-04-30 13:09:58 +0530

    change 2

--- New-Hsrp-Website/plate/src/App.jsx
+++ New-Hsrp-Website/plate/src/App.jsx
@@ -18,77 +18,92 @@ import TermsConditions from "./components/TermsConditions";
 import RefundCancellation from "./components/RefundCancellation";
 import Navbar2 from "./components/Navbar2";
 
-
-
-// TEMP pages (create later if not ready)
-const Orders = () => <div className="p-10">Orders Page</div>;
-
 function App() {
   return (
     <Router>
-      
 
       <Routes>
-        <Route
-  path="/"
-  element={
-    <>
-      <Navbar />
-      <Home />
-    </>
-  }
-/>
 
-        <Route path="/about" element={<>
-        <Navbar2 />
-        <About />
-        </>} />
-        
-        <Route path="/orders" element={<>
-        <Navbar2 />
-        <Order /></>} />
+        <Route path="/" element={
+          <>
+            <Navbar />
+            <Home />
+          </>
+        } />
+
+        <Route path="/about" element={
+          <>
+            <Navbar2 />
+            <About />
+          </>
+        } />
+
+        <Route path="/orders" element={
+          <>
+            <Navbar2 />
+            <Order />
+          </>
+        } />
+
         <Route path="/contactus" element={
           <>
-        <Navbar2 /> 
-        <Contactus />
-        </>
-        } />
-        <Route path="/installation" element={<>
-        <Navbar2 />
-        <Installation />
-        </>
-        } />
-        <Route path="/support" element={<>
-        <Navbar2 />
-        <Support />
-        </>} />
-
-        <Route path="/faq" element={<>
-        <Navbar2 />
-        <FAQ />
-        </>
-        } />
-        <Route path="/privacy-policy" element={<>
-        <Navbar2 />
-        <PrivacyPolicy />
-        </>
-        } />
-        <Route path="/terms-conditions" element={<>
-        <Navbar2 />
-        <TermsConditions />
-        </>
-        } />
-        <Route path="/refund-cancellation" element={<>
-        <Navbar2 />
-        <RefundCancellation />
-        </>
+            <Navbar2 />
+            <Contactus />
+          </>
         } />
 
-        <Route path="/booking" element={<>
-        <Navbar2 />
-        <Contact />
-        </>
+        <Route path="/installation" element={
+          <>
+            <Navbar2 />
+            <Installation />
+          </>
         } />
+
+        <Route path="/support" element={
+          <>
+            <Navbar2 />
+            <Support />
+          </>
+        } />
+
+        <Route path="/faq" element={
+          <>
+            <Navbar2 />
+            <FAQ />
+          </>
+        } />
+
+        <Route path="/privacy-policy" element={
+          <>
+            <Navbar2 />
+            <PrivacyPolicy />
+          </>
+        } />
+
+        <Route path="/terms-conditions" element={
+          <>
+            <Navbar2 />
+            <TermsConditions />
+          </>
+        } />
+
+        <Route path="/refund-cancellation" element={
+          <>
+            <Navbar2 />
+            <RefundCancellation />
+          </>
+        } />
+
+        <Route path="/booking" element={
+          <>
+            <Navbar2 />
+            <Contact />
+          </>
+        } />
+
+        {/* ✅ ADMIN ROUTE (IMPORTANT) */}
+        <Route path="/admin/login" element={<AdminLogin />} />
+
       </Routes>
 
       <Footer />
