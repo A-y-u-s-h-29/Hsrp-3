ce4899989259aabe694e7dfc32512584d53f4b18
Ayush Sharma <ayushsharma00123456@gmail.com>
2026-04-30 14:45:45 +0530

    fix mime error

--- New-Hsrp-Website/backend/server.js
+++ New-Hsrp-Website/backend/server.js
@@ -11,7 +11,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: ['https://hsrp-3.vercel.app', 'http://localhost:5173'],
+  origin: ['https://www.hsrp.site', 'http://localhost:5173'],
   credentials: true
 }));
 app.use(express.json());
